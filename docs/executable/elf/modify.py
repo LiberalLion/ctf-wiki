a14a2cb17fe55f500aecdac259f7680ee6c62232
Sourcery AI <>
2023-08-26 13:23:52 +0000

    'Refactored by Sourcery'

--- docs/executable/elf/modify.py
+++ docs/executable/elf/modify.py
@@ -1,8 +1,7 @@
 import sys
 f = open(sys.argv[1], encoding='utf-8')
-f1 = open(sys.argv[1][:-3] + '1.md', 'w', encoding='utf-8')
-for line in f.readlines():
-    if line.startswith('#'):
-        line = '#' + line
-    f1.writelines(line)
-f1.close()
+with open(f'{sys.argv[1][:-3]}1.md', 'w', encoding='utf-8') as f1:
+    for line in f:
+        if line.startswith('#'):
+            line = f'#{line}'
+        f1.writelines(line)
